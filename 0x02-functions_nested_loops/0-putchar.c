a34fecd083d97bc0f267cc69f8ed45db04c01ca6
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-21 10:30:53 -0700

    print_putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,8 @@
  * _putchar: print the alphabit putchar
  * Return: always 0
  */
-
-
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
