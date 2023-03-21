9b0b63f81b42887fd3de4c787baa2983d05b4a6a
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-21 12:14:55 -0700

    print10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - Entry point
+ *Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int x = 10, char c;
+
+	while (x = < 10)
+	{
+		c = 'a';
+		while (c = < 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+
+		_putchar('\n');
+		x++;
+	}
+}
+
