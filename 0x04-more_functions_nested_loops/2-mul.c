a17b4ee015da3815e24552697841c2c904dbaa72
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-23 09:10:02 -0700

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - computes multiplication of vars
+ * @a: firt var to multiply
+ * @b: second var to multiply
+ * Return: multiply result
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+}
