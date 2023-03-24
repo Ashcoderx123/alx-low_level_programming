75b9c02cf42503e01aa160ab8b9b65c6344deb13
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-23 18:04:05 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,7 +18,7 @@ int main(void)
 		{
 			printf(" Fizz");
 		}
-		else if (i % 3 != 0 && i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
@@ -36,5 +36,6 @@ int main(void)
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
