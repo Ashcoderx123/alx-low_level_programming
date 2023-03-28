67397681464a343daf728b8ac0084d208e8b889b
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-28 10:28:16 -0700

    str

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string of characters
+ * Return: always void
+ */
+
+void _puts(char *str)
+{
+	char str[] = "I do not fear computers. I fear the lack of them - Isaac Asimov"
+
+	puts(str);
+}
