e7e2f7240dfbe2a9e2f9df8b7ad73f040526261c
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-28 10:41:29 -0700

    revvstring

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - print a string in reverse
+ * @s: string in main function
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
