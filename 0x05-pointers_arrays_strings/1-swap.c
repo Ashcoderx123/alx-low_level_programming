2e40360ca097f99f23c3eef0dd783cad7c75554f
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-27 19:48:19 -0700

    swap_values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - change the values of a & b
+ * @a: first parameter in 1-main.c
+ * @b: second parameter in 1-main.c
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - switch the values of a & b
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
