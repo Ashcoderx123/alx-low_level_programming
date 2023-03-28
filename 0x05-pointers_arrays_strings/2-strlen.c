4f3e17791db452d3a6eee034bdedba5d6d22b1d1
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-03-28 10:00:59 -0700

    strlen1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of str
+ * @s: pointer to str
+ * Return: Always 0 success
+ */
+
+int _strlen(char *s)
+{
+	s = &str;
+	strlen(*s);
+}
