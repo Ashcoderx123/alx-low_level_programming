1ae39bb75ff5d328e2622c1bef15c1ef74d561da
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-04-05 10:42:49 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - calculate the raise to the power
+ * @x: first int to use
+ * @y: second int 
+ * Return: the result of the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (x * _pow_recursion(x, y - 1));
+}
