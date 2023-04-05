d65839c4dcd3b2acefee6f17cf9cbc483e23ccbc
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-04-05 10:46:04 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,14 +3,18 @@
 /**
  *_pow_recursion - calculate the raise to the power
  * @x: first int to use
- * @y: second int 
+ * @y: second int
  * Return: the result of the power
  */
 
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
+	{
 		return (-1);
+	}
 	if (y == 0)
+	{
 		return (x * _pow_recursion(x, y - 1));
+	}
 }
