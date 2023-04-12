75b20a58374c57eee943d9db6215b627d1531f3d
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-04-11 17:58:16 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_strdup - duplicate str
+ *@str: str to duplicate
+ *Description: copy str into newstr
+ *Return: newstr
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	char *newstr;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	newstr = malloc(sizeof(char) * (len + 1);
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		newstr[i] = str[i];
+	}
+
+	return (newstr);
+}
