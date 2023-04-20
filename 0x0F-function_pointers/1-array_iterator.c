2aa4fdd56e3f69dc9ca2168702d23fc903a8723c
Ahmed Sherif <ahmedhafezx7@outlook.com>
2023-04-19 18:33:35 -0700

    Print_array_elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - pass value of array
+ * @size: size of array
+ * @array: array to print
+ * @action: pointer to function to execute
+ *
+ * Return: void;
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
