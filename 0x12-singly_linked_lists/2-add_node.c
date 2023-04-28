af3de9642cfee1085e8b85e93bb0171b2f09ada1
Ahmed <ahmedhafezx7@outlook.com>
2023-04-27 17:49:56 -0700

    new_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *add_node - adds a new node at the beginning of a linked list
+ *@head: pointer to node head
+ *@str: string to duplicate
+ *Return: Adress of head
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node_new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	node_new = malloc(sizeof(list_t));
+
+	if (!node_new)
+	{
+		return (NULL);
+	}
+
+	node_new->str = strdup(str);
+	node_new->len = len;
+	node_new->next = (*head);
+	(*head) = node_new;
+
+	return (*head);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## Repository Overview
+This repository is part a set of low level programming as part of ALX Software Engineering Program
