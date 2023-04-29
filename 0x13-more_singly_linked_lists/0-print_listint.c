4d00d9b3fa8bbf70df4d6b96004437e11b3b0465
Ahmed <ahmedhafezx7@outlook.com>
2023-04-29 14:37:58 -0700

    debugged

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 
 /**
- *print_listint - print linked list elements
- *@h: linked list head
- *Return: number of elements
+ * print_listint - print linked list elements
+ * @h: linked list head
+ *
+ * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *itr = h;
+
 
 	while (itr != NULL)
 	{
