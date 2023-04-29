69ffbcf15fb68e2ac0e68994d9336b7031e48f90
Ahmed <ahmedhafezx7@outlook.com>
2023-04-29 14:31:49 -0700

    print_node_elements

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *print_listint - print linked list elements
+ *@h: linked list head
+ *Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *itr = h;
+
+	while (itr != NULL)
+	{
+		printf("%d\n", itr->n);
+		itr = itr->next;
+		count++;
+	}
+
+	return (count);
+}
