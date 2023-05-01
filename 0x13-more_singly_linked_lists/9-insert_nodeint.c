a1f69dd06107e55abedbf3ac8e7e106c3e7cf312
Ahmed <ahmedhafezx7@outlook.com>
2023-05-01 16:18:36 -0700

    betty 2 checked

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,38 +9,42 @@
  * Return: pointer to the new node created, or NULL
  */
 
-listint_t * insert_nodeint_at_index(listint_t ** head, unsigned int idx, int n) {
-  unsigned int x;
-  listint_t * node;
-  listint_t * cur = * head;
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int x;
+	listint_t *node;
+	listint_t *cur = *head;
 
-  node = malloc(sizeof(listint_t));
+	node = malloc(sizeof(listint_t));
 
-  if (!node || head == NULL) {
-    return (NULL);
-  }
+	if (!node || head == NULL)
+	{
+		return (NULL);
+	}
 
-  node -> n = n;
-  node -> next = NULL;
+	node->n = n;
+	node->next = NULL;
 
-  if (idx == 0) {
-    node -> next = * head;
-    * head = node;
+	if (idx == 0)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
 
-    return (node);
-  }
+	for (x = 0; cur && x < idx; x++)
+	{
+		if (x == idx - 1)
+		{
+			node->next = cur->next;
+			cur->next = node;
+			return (node);
+		}
 
-  for (x = 0; cur && x < idx; x++) {
+		else
+			cur = cur->next;
+	}
 
-    if (x == idx - 1) {
-      node -> next = cur -> next;
-      cur -> next = node;
 
-      return (node);
-    } else {
-      cur = cur -> next;
-    }
-  }
-
-  return (NULL);
+	return (NULL);
 }
---
+++ file.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function to free listint_t and set head to NULL
+ * @head: pointer to pointer to head
+ *
+ * Description: function that frees a listint_t list.
+ */
+
+void free_listint2(listint_t **head)
+{
+        listint_t *freely;
+
+        while (*head)
+        {
+                freely = (*head)->next;
+                free(*head);
+                *head = freely;
+        }
+
+        *head = NULL;
+}
+~
+~
+~
+
